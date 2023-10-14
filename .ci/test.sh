55b34b768b9114e3e2ac2ae64cdd1fcf6f5e35fb
Maksim Bondarenkov <119937608+ognevnydemon@users.noreply.github.com>
2023-10-14 12:05:09 +0300

    fix test

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/sh
 
-./target/debug/fkysoxide main.fkys > output.txt
-if [ "$(cat output.txt)" = "Hello, world!\n" ]; then
-    echo "failed with $(cat output.txt)"
-    rm -f output.txt
+output="$(./target/debug/fkysoxide main.fkys)"
+if [ $output != "Hello, world!" ]; then
+    echo "failed with $output"
     exit 1
 fi
-rm -f output.txt
