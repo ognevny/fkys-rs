c8914bbf6370137a3d5fad97f548062b1fc4b783
Maksim Bondarenkov <119937608+ognevnydemon@users.noreply.github.com>
2023-10-14 12:15:26 +0300

    another test.sh fix

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 output="$(./target/debug/fkysoxide main.fkys)"
-if [ $output != "Hello, world!" ]; then
+if [[ $output != "Hello, world!" ]]; then
     echo "failed with $output"
     exit 1
 fi
