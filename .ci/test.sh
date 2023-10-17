c265a971f60c849a2e5c323d34fe0ffabf710aa8
ognevnydemon <maksapple2306@gmail.com>
2023-10-17 19:22:37 +0300

    test now works

--- .ci/test.sh
+++ .ci/test.sh
@@ -2,8 +2,8 @@
 
 output="$(./target/debug/fkysoxide main.fkys)"
 if [[ $output != "Hello, world!" ]]; then
-    echo "failed with $output"
+    printf "%s\n%s\n%s\n%s\n" "failed with" "~~~~~~~~" "$output" "~~~~~~~~"
     exit 1
 fi
 
-echo "$output"
+printf "\n%s\n" "$output"
--- tests/test.rs
+++ tests/test.rs
@@ -1,16 +1,9 @@
 #[test]
 fn hw() {
-    let command = std::process::Command::new("sh")
+    let stat = std::process::Command::new("sh")
         .arg("./.ci/test.sh")
-        .output()
+        .status()
         .expect("failed to execute");
-    if !command.status.success() {
-        println!(
-            "failed with {}\ncode {}",
-            String::from_utf8_lossy(&command.stdout),
-            command.status.code().unwrap_or(-1)
-        );
-    } else {
-        println!("success! {}", String::from_utf8_lossy(&command.stdout));
-    }
+    
+    assert!(stat.success())
 }
